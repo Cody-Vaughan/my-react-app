5ac07f547dbd592d553a8d51ebe9e9be5dd69505
Cody <c.j.vaughan@wustl.edu>
2025-11-09 16:12:56 -0600

    gave each div the class "board-row" so they are affected by the css properly as given by the tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 export default function Square() {
   return(
     <>
-    <div>
+    <div class="board-row">
       <button className="square">X</button> 
       <button className="square">X</button>
       <button className="square">X</button> 
     </div>
-      <div>
+      <div class="board-row">
       <button className="square">X</button> 
       <button className="square">X</button>
       <button className="square">X</button> 
     </div>
-      <div>
+      <div class="board-row">
       <button className="square">X</button> 
       <button className="square">X</button>
       <button className="square">X</button> 
