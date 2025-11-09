e5a422af9e839c7188541a532ffc76c864aa78ef
Cody <c.j.vaughan@wustl.edu>
2025-11-09 17:08:19 -0600

    added in code at the start of board which displays the status of the game which is above the tictactoe board in a new div I added with a className of status

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,14 @@ export default function Board() {
 
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
     function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -34,20 +42,27 @@ export default function Board() {
 
   return(
     <>
+    <div className="status">{status}</div>
     <div class="board-row">
+
       <Square value={squares[0]}  onSquareClick={() => handleClick(0)}/>
       <Square value={squares[1]}  onSquareClick={() => handleClick(1)}/>
       <Square value={squares[2]}  onSquareClick={() => handleClick(2)}/>
+
     </div>
       <div class="board-row">
+
         <Square value={squares[3]}  onSquareClick={() => handleClick(3)}/>
         <Square value={squares[4]}  onSquareClick={() => handleClick(4)}/>
         <Square value={squares[5]}  onSquareClick={() => handleClick(5)}/>
+
     </div>
       <div class="board-row">
+
         <Square value={squares[6]}  onSquareClick={() => handleClick(6)}/>
         <Square value={squares[7]}  onSquareClick={() => handleClick(7)}/>
         <Square value={squares[8]}  onSquareClick={() => handleClick(8)}/>
+
     </div>
     </>
 
